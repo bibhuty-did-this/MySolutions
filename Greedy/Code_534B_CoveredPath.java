9f2c7a8b3adaefb43218ada0aff9cd8e14a04299
Bibhuti Bhusan Panda <emailofpanda@yahoo.com>
2017-09-12 01:19:28 +0530

    Code_534B_CoveredPath

---
+++ Greedy/Code_534B_CoveredPath.java
@@ -0,0 +1,133 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author o_panda_o(emailofpanda@yahoo.com)
+ */
+public class Code_534B_CoveredPath{
+    public static void main(String[] args){
+        InputStream inputStream=System.in;
+        OutputStream outputStream=System.out;
+        InputReader in=new InputReader(inputStream);
+        OutputWriter out=new OutputWriter(outputStream);
+        _534B_ solver=new _534B_();
+        solver.solve(1,in,out);
+        out.close();
+    }
+
+    static class _534B_{
+        public void solve(int testNumber,InputReader in,OutputWriter out){
+            int a=in.nextInt(), b=in.nextInt(), t=in.nextInt(), d=in.nextInt();
+            int v1=Math.min(a,b), v2=Math.max(a,b), max=0;
+            for(int t1=1;t1<=t-1;++t1)
+                if(Math.abs(v1+(t1-1)*d-(v2+(t-t1-1)*d))<=d)
+                    max=Math.max(((2*v1+(t1-1)*d)*t1)/2+((2*v2+(t-t1-1)*d)*(t-t1))/2,max);
+
+            out.println(max);
+        }
+
+    }
+
+    static class OutputWriter{
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream){
+            writer=new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer){
+            this.writer=new PrintWriter(writer);
+        }
+
+        public void close(){
+            writer.close();
+        }
+
+        public void println(int i){
+            writer.println(i);
+        }
+
+    }
+
+    static class InputReader{
+        private InputStream stream;
+        private byte[] buf=new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream){
+            this.stream=stream;
+        }
+
+        public int read(){
+            if(numChars==-1){
+                throw new InputMismatchException();
+            }
+            if(curChar>=numChars){
+                curChar=0;
+                try{
+                    numChars=stream.read(buf);
+                }catch(IOException e){
+                    throw new InputMismatchException();
+                }
+                if(numChars<=0){
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt(){
+            int c=read();
+            while(isSpaceChar(c)){
+                c=read();
+            }
+            int sgn=1;
+            if(c=='-'){
+                sgn=-1;
+                c=read();
+            }
+            int res=0;
+            do{
+                if(c<'0'||c>'9'){
+                    throw new InputMismatchException();
+                }
+                res*=10;
+                res+=c-'0';
+                c=read();
+            }while(!isSpaceChar(c));
+            return res*sgn;
+        }
+
+        public boolean isSpaceChar(int c){
+            if(filter!=null){
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c){
+            return c==' '||c=='\n'||c=='\r'||c=='\t'||c==-1;
+        }
+
+        public interface SpaceCharFilter{
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
+
