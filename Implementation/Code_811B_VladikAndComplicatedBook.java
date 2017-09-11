36638b064fc2a7ad08030944f7b4eacfc3f00bab
Bibhuti Bhusan Panda <emailofpanda@yahoo.com>
2017-09-11 15:55:17 +0530

    Code_811B_VladikAndComplicatedBook

---
+++ Implementation/Code_811B_VladikAndComplicatedBook.java
@@ -0,0 +1,149 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author o_panda_o(emailofpanda@yahoo.com)
+ */
+public class Code_811B_VladikAndComplicatedBook{
+    public static void main(String[] args){
+        InputStream inputStream=System.in;
+        OutputStream outputStream=System.out;
+        InputReader in=new InputReader(inputStream);
+        OutputWriter out=new OutputWriter(outputStream);
+        _811B_ solver=new _811B_();
+        solver.solve(1,in,out);
+        out.close();
+    }
+
+    static class _811B_{
+        public void solve(int testNumber,InputReader in,OutputWriter out){
+            int n=in.nextInt(), m=in.nextInt(), p[]=in.nextIntArray(n);
+            while(m-->0){
+                int l=in.nextInt()-1, r=in.nextInt()-1, x=in.nextInt()-1, less=0;
+                for(int i=l;i<=r;less+=(p[i++]<p[x]?1:0)) ;
+                if(less==x-l) out.println("Yes");
+                else out.println("No");
+            }
+        }
+
+    }
+
+    static class InputReader{
+        private InputStream stream;
+        private byte[] buf=new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream){
+            this.stream=stream;
+        }
+
+        public int read(){
+            if(numChars==-1){
+                throw new InputMismatchException();
+            }
+            if(curChar>=numChars){
+                curChar=0;
+                try{
+                    numChars=stream.read(buf);
+                }catch(IOException e){
+                    throw new InputMismatchException();
+                }
+                if(numChars<=0){
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt(){
+            int c=read();
+            while(isSpaceChar(c)){
+                c=read();
+            }
+            int sgn=1;
+            if(c=='-'){
+                sgn=-1;
+                c=read();
+            }
+            int res=0;
+            do{
+                if(c<'0'||c>'9'){
+                    throw new InputMismatchException();
+                }
+                res*=10;
+                res+=c-'0';
+                c=read();
+            }while(!isSpaceChar(c));
+            return res*sgn;
+        }
+
+        public boolean isSpaceChar(int c){
+            if(filter!=null){
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c){
+            return c==' '||c=='\n'||c=='\r'||c=='\t'||c==-1;
+        }
+
+        public int[] nextIntArray(int n){
+            int[] array=new int[n];
+            for(int i=0;i<n;++i) array[i]=nextInt();
+            return array;
+        }
+
+        public interface SpaceCharFilter{
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+
+    static class OutputWriter{
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream){
+            writer=new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer){
+            this.writer=new PrintWriter(writer);
+        }
+
+        public void print(Object... objects){
+            for(int i=0;i<objects.length;i++){
+                if(i!=0){
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void println(Object... objects){
+            print(objects);
+            writer.println();
+        }
+
+        public void close(){
+            writer.close();
+        }
+
+    }
+}
+
