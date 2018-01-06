0bf3966b21ece0ecd396a9c072dd41be842df69d
TheEfficientCodes <emailofpanda@yahoo.com>
2018-01-06 18:26:48 +0530

    Code_897A_ScarboroughFair

---
+++ Implementation/Code_897A_ScarboroughFair.java
@@ -0,0 +1,163 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author o_panda_o(emailofpanda @ yahoo.com)
+ */
+public class Code_897A_ScarboroughFair{
+    public static void main(String[] args){
+        InputStream inputStream=System.in;
+        OutputStream outputStream=System.out;
+        InputReader in=new InputReader(inputStream);
+        OutputWriter out=new OutputWriter(outputStream);
+        _897A_ solver=new _897A_();
+        solver.solve(1,in,out);
+        out.close();
+    }
+
+    static class _897A_{
+        public void solve(int testNumber,InputReader in,OutputWriter out){
+            int n=in.nextInt(), m=in.nextInt();
+            char[] string=in.next().toCharArray();
+            while(m-->0){
+                int l=in.nextInt()-1, r=in.nextInt()-1;
+                char c1=in.nextCharacter(), c2=in.nextCharacter();
+                for(int i=l;i<=r;++i)
+                    if(string[i]==c1)
+                        string[i]=c2;
+            }
+            out.print(string);
+        }
+
+    }
+
+    static class OutputWriter{
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream){
+            writer=new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer){
+            this.writer=new PrintWriter(writer);
+        }
+
+        public void print(char[] array){
+            writer.print(array);
+        }
+
+        public void close(){
+            writer.close();
+        }
+
+    }
+
+    static class InputReader{
+        private InputStream stream;
+        private byte[] buf=new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream){
+            this.stream=stream;
+        }
+
+        public int read(){
+            if(numChars==-1){
+                throw new InputMismatchException();
+            }
+            if(curChar >= numChars){
+                curChar=0;
+                try{
+                    numChars=stream.read(buf);
+                }catch(IOException e){
+                    throw new InputMismatchException();
+                }
+                if(numChars<=0){
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt(){
+            int c=read();
+            while(isSpaceChar(c)){
+                c=read();
+            }
+            int sgn=1;
+            if(c=='-'){
+                sgn=-1;
+                c=read();
+            }
+            int res=0;
+            do{
+                if(c<'0'||c>'9'){
+                    throw new InputMismatchException();
+                }
+                res*=10;
+                res+=c-'0';
+                c=read();
+            }while(!isSpaceChar(c));
+            return res*sgn;
+        }
+
+        public String nextString(){
+            int c=read();
+            while(isSpaceChar(c)){
+                c=read();
+            }
+            StringBuilder res=new StringBuilder();
+            do{
+                if(Character.isValidCodePoint(c)){
+                    res.appendCodePoint(c);
+                }
+                c=read();
+            }while(!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c){
+            if(filter!=null){
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c){
+            return c==' '||c=='\n'||c=='\r'||c=='\t'||c==-1;
+        }
+
+        public char nextCharacter(){
+            int c=read();
+            while(isSpaceChar(c)){
+                c=read();
+            }
+            return (char)c;
+        }
+
+        public String next(){
+            return nextString();
+        }
+
+        public interface SpaceCharFilter{
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
+
