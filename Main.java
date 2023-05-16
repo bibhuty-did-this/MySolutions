69d7397676a2bb53c35d23a0bd1b3d64a324bb44
bibhuty-did-this <bibhuty.nit@gmail.com>
2023-05-16 12:45:59 +0530

    Chaning in main

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 public class Main {
     public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
 
+        out.close();
     }
 }
